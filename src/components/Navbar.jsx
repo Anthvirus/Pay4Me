2e78b8fecd7627dacd6d6183c64ff298352f287d
Ahntoni <76910782+Anthvirus@users.noreply.github.com>
2025-01-28 17:18:36 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
                               <img src={Logo} alt="Pay4Me Logo" className="w-48 md:w-48 xl:w-80 sm:w-40 xl:-ml-24"/>
                         </div>
                         <div>
-                              <button className="py-4 px-5 bg-white text-sm lg:text-[1rem]  rounded-md shadow-sm shadow-gray-500 text-[#0A5D2A] font-semibold h-auto hover:scale-[85%] lg:hover:scale-95 lg:scale-100 scale-90">Download App</button>
+                              <button className="py-4 px-5 bg-white text-sm lg:text-[1rem]  rounded-md shadow-sm shadow-gray-500 text-[#0A5D2A] font-semibold h-auto hover:scale-[75%] lg:hover:scale-95 lg:scale-100 scale-[80%]">Download App</button>
                         </div>
                   </nav>
             </>
