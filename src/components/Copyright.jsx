fed32fe09be6e15efe02f458e0b2fd34b925365d
Ahntoni <76910782+Anthvirus@users.noreply.github.com>
2025-01-29 17:39:44 +0100

    Update Copyright.jsx

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -2,8 +2,9 @@ export default function Copyright() {
           var year = new Date().getFullYear()
           return (
                     <>
-                              <section className="flex w-full h-20 text-sm text-white bg-black xl:-mt-40 md:text-xl">
-                                        <p className="m-auto"> Copyright. &copy; {year} all right reserved Pay4Me</p>  
+                              <section className="flex w-full h-20 text-sm text-white bg-black xl:-mt-40 md:text-xl justify-center items-center">
+                                        <p> Copyright. &copy; {year} all right reserved Pay4Me</p>
+                                        <p>UI by Dami.</p>
                               </section>
                     </>
           )
